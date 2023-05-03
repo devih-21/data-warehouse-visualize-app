7b18a9f9da95ffe1c1c024eeadf506dd7cc5a147
devih-rs <vinh@reelsights.com>
2023-05-03 13:20:47 +0700

    feat: init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import PivotGrid, {
+  FieldChooser,
+} from 'devextreme-react/pivot-grid';
+
+import PivotGridDataSource from 'devextreme/ui/pivot_grid/data_source';
+import XmlaStore from 'devextreme/ui/pivot_grid/xmla_store';
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="long-title">
+          <h3>Sales Statistics</h3>
+        </div>
+        <PivotGrid
+          dataSource={dataSource}
+          allowSortingBySummary={true}
+          allowSorting={true}
+          allowFiltering={true}
+          allowExpandAll={true}
+          height={570}
+          showBorders={true}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <FieldChooser enabled={true} allowSearch={true} />
+        </PivotGrid>
+      </React.Fragment>
+    );
+  }
 }
+
+const dataSource = new PivotGridDataSource({
+  fields: [
+    { dataField: '[Product].[Category]', area: 'row' },
+    {
+      dataField: '[Product].[Subcategory]',
+      area: 'row',
+      headerFilter: {
+        allowSearch: true,
+      },
+    },
+    { dataField: '[Ship Date].[Calendar Year]', area: 'column' },
+    { dataField: '[Ship Date].[Month of Year]', area: 'column' },
+    { dataField: '[Measures].[Reseller Freight Cost]', area: 'data', format: 'currency' },
+  ],
+  store: new XmlaStore({
+    type: 'xmla',
+    url: 'https://demos.devexpress.com/Services/OLAP/msmdpump.dll',
+    catalog: 'Adventure Works DW Standard Edition',
+    cube: 'Adventure Works',
+  }),
+});
 
 export default App;
